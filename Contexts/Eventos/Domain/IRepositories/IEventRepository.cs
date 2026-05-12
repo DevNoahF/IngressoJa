a0f33bb043dae40e694018dc63e7f6ce51d3fc3d
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-05-12 20:28:55 -0300

    fix/added getEventByNameUseCase

--- Contexts/Eventos/Domain/IRepositories/IEventRepository.cs
+++ Contexts/Eventos/Domain/IRepositories/IEventRepository.cs
@@ -9,5 +9,6 @@ public interface IEventRepository
     Task<EventPutResponseDTO> UpdateEvent(EventPutRequestDTO eventPutRequestDto);
     Task<IEnumerable<EventSummaryResponseDTO>> GetAllEvents();//Pega todos os eventos
     Task<EventDetailResponseDTO> GetEventById(Guid id);//Pega Somente 1 evento 
+    Task<EventSummaryResponseDTO> GetEventByName(string name);
     
 }
