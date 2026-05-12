6088ef524250dfcbe96f3f880ab2f0ce794deec1
noah.franco <noah.franco@fulltime.com.br>
2026-05-12 14:59:12 -0300

    feat: enhance user authentication by implementing login and user retrieval methods

--- Contexts/Eventos/Application/Interfaces/User/IUserUseCase.cs
+++ Contexts/Eventos/Application/Interfaces/User/IUserUseCase.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using IngressoJa.Contexts.Eventos.Application.DTOs.Request;
 using IngressoJa.Contexts.Eventos.Application.DTOs.Response.User;
-
+using IngressoJa.Contexts.Eventos.Application.DTOs.Response;
+using IngressoJa.Contexts.Eventos.Domain.Entities;
+using IngressoJa.Contexts.Eventos.Domain.Entities.ValueObject;
 namespace IngressoJa.Contexts.Eventos.Application.Interfaces.User
 {
     public interface IUserUseCase
     {
         Task RegisterUser(UserRegisterRequestDTO userRegisterRequestDTO);
-        Task LoginUser(UserAuthRequestDTO userAuthRequestDTO);
+        Task<UserAuthResponseDTO> LoginUser(UserAuthRequestDTO userAuthRequestDTO);
         Task<UserRecordedResponseDTO> getUser(Guid id);
+        Task<UserEntity> getUserByEmail(EmailVO email);
     }
 }
