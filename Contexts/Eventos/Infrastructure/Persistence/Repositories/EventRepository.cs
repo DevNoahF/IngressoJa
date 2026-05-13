5d8b29194adc63df9264b1251e3e0ad89b36ebaf
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-05-12 21:59:07 -0300

    Changed Folder Structure

---
+++ Contexts/Eventos/Infrastructure/Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,13 @@
+﻿using IngressoJa.Contexts.Eventos.Domain.IRepositories;
+
+namespace IngressoJa.Contexts.Eventos.Infrastructure.Persistence.Repositories;
+
+public class EventRepository: IEventRepository
+{
+    private readonly AppDbContext _context;
+
+    public EventRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+}
