12eeb2232612c2df7dd5e2ec51cb6ce8d7c4d820
noah.franco <noah.franco@fulltime.com.br>
2026-05-20 14:49:33 -0300

    feat: update UserRecordedResponseDTO to include first and last name, and additional value objects

--- Contexts/Eventos/Adapters/DTOs/Response/User/UserRecordedResponseDTO.cs
+++ Contexts/Eventos/Adapters/DTOs/Response/User/UserRecordedResponseDTO.cs
@@ -2,13 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IngressoJa.Contexts.Eventos.Domain.Entities.ValueObject;
 
 namespace IngressoJa.Contexts.Eventos.Application.DTOs.Response.User
 {
     public record UserRecordedResponseDTO(
         Guid Id,
-        String CompleteName,
-        String Email,
+        String FirstName,
+        String LastName,
+        CpfVO Cpf,
+        EmailVO Email,
+        PhotoProfileVO PhotoProfile,
         DateTime DateBirth
     );
 }
