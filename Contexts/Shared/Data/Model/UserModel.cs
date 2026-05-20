8e120593bbbf079ab4a5f8c2c9de7166965d10f3
noah.franco <noah.franco@fulltime.com.br>
2026-05-20 14:49:26 -0300

    refactor: clean up UserModel by removing unnecessary whitespace and organizing properties

--- Contexts/Shared/Data/Model/UserModel.cs
+++ Contexts/Shared/Data/Model/UserModel.cs
@@ -11,17 +11,13 @@ namespace IngressoJa.Data.Model
     {
         public Guid Id { get;  set; }
         public RoleEnum Role { get;  set; }
-
         public String FirstName { get; set; }
         public String LastName { get; set; }
-
         public CpfVO Cpf { get; set; }
         public EmailVO Email { get; set; }
-        
+        public PhotoProfileVO PhotoProfile { get; set; }
         public PasswordVO PasswordHash { get; set; }
-
         public String Token { get; set; }
-
         public DateTime DateBirth { get; set; }
     }
 }
