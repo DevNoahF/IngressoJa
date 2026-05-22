db757ef9b6d9cdd967426c4ce7e93a3359f8406d
noah <noahvf16@outlook.com>
2026-05-21 21:35:42 -0300

    refactor: remove unused EventDbContext registration

--- Program.cs
+++ Program.cs
@@ -32,8 +32,6 @@ builder.Services.AddScoped<GetSaleByIdUseCase>();
 builder.Services.AddScoped<UpdateSaleStatusUseCase>();
 
 // Eventos
-builder.Services.AddDbContext<EventDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("EventosConnection")));
 builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<CreateEventUseCase>();
 builder.Services.AddScoped<DeleteEventUseCase>();
