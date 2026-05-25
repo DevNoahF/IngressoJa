308bfa46fac2f54f24b5fe5bf0065311a1d6ff21
Joaquim_Oliveira <yunohanayuunasan@gmail.com>
2026-05-24 22:07:05 -0300

    feat: add TicketModel

---
+++ BackEnd/Contexts/Shared/Data/Model/TicketModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace IngressoJa.Contexts.Shared.Data.Model
+{
+    public class TicketModel
+    {
+        public Guid Codigo { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
