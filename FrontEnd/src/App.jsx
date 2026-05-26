f11e4bb93cd2c17aff36e000af31a61cdd7c4574
Joaquim_Oliveira <yunohanayuunasan@gmail.com>
2026-05-26 20:39:28 -0300

    feat: alterações em ChangeEventStatus e relacionadas

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,10 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
+import ChangeEventStatus from './pages/changeEventStatus/ChangeEventStatusPage'
 import Home from './pages/home/HomePage'
 import CreateEvent from './pages/createEvent/CreateEvent'
-import Register from "./pages/register/Register";
-import Login from "./pages/login/LoginPage";
-import Payment from './pages/Payment/PaymentPage';
+import Register from "./pages/register/Register"
+import Login from "./pages/login/LoginPage"
+import Payment from './pages/Payment/PaymentPage'
 import './App.css'
 import { canCreateEvent } from './utils/auth'
 
@@ -11,10 +12,8 @@ function RequireCreateEventAccess({ children }) {
   if (!canCreateEvent()) {
     return <Navigate to='/' replace />
   }
-
   return children
 }
-
 
 function App() {
   return (
@@ -23,18 +22,15 @@ function App() {
       <Route path='/register' element={<Register />} />
       <Route path='/home' element={<Home />} />
       <Route path='/payment' element={<Payment />} />
-      <Route
-        path='/create-event'
-        element={(
-          <RequireCreateEventAccess>
-            <CreateEvent />
-          </RequireCreateEventAccess>
-        )}
-      />
+      <Route path='/change-event-status' element={<ChangeEventStatus />} />
+      <Route path='/create-event' element={
+        <RequireCreateEventAccess>
+          <CreateEvent />
+        </RequireCreateEventAccess>
+      } />
       <Route path='*' element={<Navigate to='/login' replace />} />
     </Routes>
   )
 }
-
 
 export default App
