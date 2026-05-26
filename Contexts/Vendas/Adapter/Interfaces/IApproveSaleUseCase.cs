f2a8cd398bf9754686ace3f4c918127b295cdd2a
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-05-25 21:54:05 -0300

    fix: remover context outside backent

--- Contexts/Vendas/Adapter/Interfaces/IApproveSaleUseCase.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace IngressoJa.Contexts.Vendas.Domain.UseCases.ApproveSale
-{
-    public interface IApproveSaleUseCase
-    {
-        Task ExecuteAsync(int saleId);
-    }
-}
