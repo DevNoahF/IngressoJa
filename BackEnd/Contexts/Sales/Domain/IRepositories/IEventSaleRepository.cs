1ab209ec29f0252fc575822910f21a617ab16aa4
Joaquim_Oliveira <yunohanayuunasan@gmail.com>
2026-05-26 21:08:49 -0300

    feat: generate ticket only when sale is approved

--- BackEnd/Contexts/Sales/Domain/IRepositories/IEventSaleRepository.cs
+++ BackEnd/Contexts/Sales/Domain/IRepositories/IEventSaleRepository.cs
@@ -7,6 +7,8 @@ public interface IEventSaleRepository
     Task<EventSaleEntity> AddEvent(EventSaleEntity eventSaleEntity);
     Task<IEnumerable<EventSaleEntity>> GetAllEvents();
     Task<EventSaleEntity?> GetEventSaleById(Guid id);
+    Task<EventSaleEntity?> GetByEventIdAsync(Guid eventId);
     Task<EventSaleEntity> UpdateEvent(EventSaleEntity eventSaleEntity);
+    Task<EventSaleEntity> UpdateAsync(EventSaleEntity eventSaleEntity);
     Task DeleteEvent(Guid id);
 }
