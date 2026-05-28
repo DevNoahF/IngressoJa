d59398b06b54e71bbd551db8d76153faa8900964
Joaquim_Oliveira <yunohanayuunasan@gmail.com>
2026-05-28 20:37:46 -0300

    feat: add auth guard to ChangeEventStatus route

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -6,9 +6,14 @@ import Register from "./pages/register/Register"
 import Login from "./pages/login/LoginPage"
 import Payment from './pages/Payment/PaymentPage'
 import './App.css'
-import { canCreateEvent } from './utils/auth'
-import UpdateProfile from './pages/updateUser/updateUser'
-import OrganizerEvents from './pages/organizerEvents/OrganizerEvents'
+import { canCreateEvent, getStoredToken } from './utils/auth'
+
+function RequireAuth({ children }) {
+  if (!getStoredToken()) {
+    return <Navigate to='/login' replace />
+  }
+  return children
+}
 
 function RequireCreateEventAccess({ children }) {
   if (!canCreateEvent()) {
@@ -20,22 +25,20 @@ function RequireCreateEventAccess({ children }) {
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Navigate to='/login' replace />} />
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
       <Route path='/home' element={<Home />} />
-      <Route path='/user/home' element={<Home />} />
       <Route path='/payment' element={<Payment />} />
-      <Route path='/user/payment' element={<Payment />} />
-      <Route path='/change-event-status' element={<ChangeEventStatus />} />
+      <Route path='/change-event-status' element={
+        <RequireAuth>
+          <ChangeEventStatus />
+        </RequireAuth>
+      } />
       <Route path='/create-event' element={
         <RequireCreateEventAccess>
           <CreateEvent />
         </RequireCreateEventAccess>
       } />
-      <Route path='/organizer/create' element={<CreateEvent />} />
-      <Route path='/organizer/home' element={<OrganizerEvents />} />
-      <Route path='/update' element={<UpdateProfile />} />
       <Route path='*' element={<Navigate to='/login' replace />} />
     </Routes>
   )
