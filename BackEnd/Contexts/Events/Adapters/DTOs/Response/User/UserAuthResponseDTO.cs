e05eb2d1dc3b103601e6a73236354d185f649c64
DevNoahF <noahvf16@outlook.com>
2026-06-06 14:35:07 -0300

    feat: add user role to UserAuthResponseDTO and update UserMapper fix: adjust MySQL port and enhance healthcheck in docker-compose

--- BackEnd/Contexts/Events/Adapters/DTOs/Response/User/UserAuthResponseDTO.cs
+++ BackEnd/Contexts/Events/Adapters/DTOs/Response/User/UserAuthResponseDTO.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IngressoJa.Contexts.Eventos.Domain.Entities.Enums;
 using IngressoJa.Contexts.Eventos.Domain.Entities.ValueObject;
 
 namespace IngressoJa.Contexts.Eventos.Application.DTOs.Response
 {
     public record UserAuthResponseDTO(
         Guid Id,
+        RoleEnum role,
         String Token,
         String FistName,
         PhotoProfileVO PhotoProfile
